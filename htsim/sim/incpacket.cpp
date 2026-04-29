8ed07a5300778b3d3e57bb3fec82dbefb4171443
Chiara Di Stefano <164644307+chiaradistefano@users.noreply.github.com>
2026-04-29 14:22:12 +0200

    Update comments incpacket.cpp
    Signed-off-by: Chiara Di Stefano <164644307+chiaradistefano@users.noreply.github.com>

--- htsim/sim/incpacket.cpp
+++ htsim/sim/incpacket.cpp
@@ -1,6 +1,6 @@
 // -*- c-basic-offset: 4; indent-tabs-mode: nil -*-
 #include "incpacket.h"
 
-// Inizializzazione del database statico per il riciclo dei pacchetti
+// Static database initialization for packet recycling
 PacketDB<IncPacket> IncPacket::_packetdb;
 PacketFlow IncPacket::_inc_flow(nullptr);

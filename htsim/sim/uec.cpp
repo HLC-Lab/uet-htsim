b18eef4456d9535a7b432b8c8348f5742d798c51
Chiara Di Stefano <164644307+chiaradistefano@users.noreply.github.com>
2026-05-02 17:18:52 +0200

    update uec.cpp
    Signed-off-by: Chiara Di Stefano <164644307+chiaradistefano@users.noreply.github.com>

--- htsim/sim/uec.cpp
+++ htsim/sim/uec.cpp
@@ -1387,7 +1387,6 @@ void UecSrc::dontUpdateCwndOnNack(bool skip, mem_b nacked_bytes, bool last_hop) 
 }
 
 void UecSrc::update_base_rtt(simtime_picosec raw_rtt){
-    if (_dstaddr == UINT32_MAX) return;
     if (_base_rtt > raw_rtt) {
         _base_rtt = raw_rtt;
         _bdp = timeAsUs(raw_rtt) * _nic.linkspeed() / 8000000; 
@@ -3169,5 +3168,3 @@ void UecPullPacer::requestPull(UecSink* sink) {
         _active = true;
     }
 }
-
-

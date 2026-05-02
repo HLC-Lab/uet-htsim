f5ac3067a10d3fcd110c0e47a7303bac9f768c74
Chiara Di Stefano <164644307+chiaradistefano@users.noreply.github.com>
2026-05-02 18:17:30 +0200

    update inc_switch.h
    Signed-off-by: Chiara Di Stefano <164644307+chiaradistefano@users.noreply.github.com>

--- htsim/sim/datacenter/inc_switch.h
+++ htsim/sim/datacenter/inc_switch.h
@@ -2,27 +2,27 @@
 #ifndef _INCSWITCH_H
 #define _INCSWITCH_H
 
-#include "fat_tree_switch.h"
+#include "inc_fat_tree_switch.h"
 #include "incpacket.h"
 #include <map>
 #include <set>
 #include <vector>
 
-// Struttura per tracciare lo stato dell'aggregazione
+// Structure to trace the state of aggregation
 struct AggregationEntry {
     std::set<uint32_t> received_flows; 
     simtime_picosec first_arrival;
     uint32_t aggregated_data = 0;
 };
 
-class IncSwitch : public FatTreeSwitch {
+class IncSwitch : public IncFatTreeSwitch {
 public:
-    IncSwitch(EventList& eventlist, string s, switch_type t, uint32_t id, simtime_picosec delay, FatTreeTopology* ft);
+    IncSwitch(EventList& eventlist, string s, switch_type t, uint32_t id, simtime_picosec delay, IncFatTreeTopology* ft);
 
-    // Override del metodo principale di ricezione
+    // Override of receiving method
     void receivePacket(Packet& pkt) override;
 
-    // Metodi specifici per INC
+    // Method for INC
     bool hasUpLinks();
     void handle_inc_packet(Packet* p);
     void send_aggregated_packet(uint32_t job_id, uint32_t block_id, uint32_t aggregated_data, uint32_t pkt_size);
@@ -33,11 +33,11 @@ public:
     vector<int> get_multicast_ports(const vector<uint32_t>& participants);
 
 private:
-    // Tabelle di stato specifiche per questa istanza di switch
+    // State tables specific to this switch instance 
     std::map<std::pair<uint32_t, uint32_t>, AggregationEntry> _aggregation_table;
     std::set<std::pair<uint32_t, uint32_t>> _completed_blocks;
 
-    // Helper interni
+    // Internal helper
     int calculate_expected_children();
     bool is_destination_downstream(uint32_t dest_id);
     vector<int> get_multicast_ports();
